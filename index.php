6785e7f8372a2af2cd019bf3cafb7d45b335674f
AD11 Koddojo <dojo@kgm.cc>
2011-10-28 13:05:10 +0200

    Katarina försöker göra en ny array av den ursprungliga arrayen

--- index.php
+++ index.php
@@ -8,7 +8,32 @@
  
 // vår indata till programmet
 $input_string = "**.|…|…";
-$answer_string = "**1|221|000";
+$answer_string="**1|221|000";
+
+function programmet($input)
+{
+	//gör input till en array
+	$test=explode("|",$input);
+	print_r($test);
+
+	$test2=array();
+	//Dela upp arrayen i underarrayer
+	for($i=0;$i<count($test);$i++)
+		{
+		echo $test[$i]."hej";
+		for($j=0;$j<strlen($test[$i]);$j++)
+			$test2[$i]=substr($test[$j],$j,1);
+			{
+			$test[$i][$j]=substr($test[$j],$j,1);
+		}
+	}
+	echo $test[0][0]."då";
+	print_r($test);
+	//in med en sträng, ut med en sträng
+	$output=$answer_string;
+	echo $output;
+	return $output;
+}
 
 // kör "programmet"
 $output_string = programmet( $input_string );
